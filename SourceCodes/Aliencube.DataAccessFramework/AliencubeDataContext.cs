31bd68f9e5189b9c4b528ba9166f8d65bffcc116
Justin Yoo <justin.yoo@aliencube.com>
2013-11-08 16:11:37 +1100

    Added Entity Framework NuGet package library files.

--- SourceCodes/Aliencube.DataAccessFramework/AliencubeDataContext.cs
+++ SourceCodes/Aliencube.DataAccessFramework/AliencubeDataContext.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,78 @@ namespace Aliencube.DataAccessFramework
 {
     public class AliencubeDataContext : DbContext, IAliencubeDataContext
     {
+        #region Constructors
+
+        /// <summary>
+        /// Initialises a new instance of the AliencubeDataContext object.
+        /// </summary>
+        public AliencubeDataContext()
+            : base("Aliencube")
+        {
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the AliencubeDataContext object.
+        /// </summary>
+        /// <param name="connectionString">SQL database connection string.</param>
+        public AliencubeDataContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the AliencubeDataContext object.
+        /// </summary>
+        /// <param name="connection">SQL database connection</param>
+        public AliencubeDataContext(DbConnection connection)
+            : base(connection, true)
+        {
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// This method is called when the model for a derived context has been initialized,
+        /// but before the model has been locked down and used to initialize the context.
+        /// The default implementation of this method does nothing, but it can be overridden
+        /// in a derived class such that the model can be further configured before it
+        /// is locked down.
+        /// </summary>
+        /// <param name="modelBuilder">The builder that defines the model for the context being created.</param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //  Adds configurations automatically by scanning assembly.
+            //  http://romiller.com/2012/03/26/dynamically-building-a-model-with-code-first
+
+            //  Gets the MethodInfo instance for "Add".
+            var addMethod = (typeof(ConfigurationRegistrar))
+                .GetMethods()
+                .Single(m => m.Name == "Add" && m.GetGenericArguments()
+                                                 .Any(a => a.Name == "TEntityType"));
+
+            //  Gets an assembly that contains entity type configurations.
+            var assembly = AppDomain.CurrentDomain
+                                    .GetAssemblies()
+                                    .Single(p => p.GetName().Name.StartsWith("Aliencube.DataEntities"));
+
+            //  Adds all types found within the assembly.
+            foreach (var type in assembly.GetTypes()
+                                         .Where(t => t.BaseType != null
+                                                     && t.BaseType.IsGenericType
+                                                     && t.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>)))
+            {
+                var entityType = type.BaseType.GetGenericArguments().Single();
+                var entityConfig = assembly.CreateInstance(type.FullName);
+
+                addMethod.MakeGenericMethod(entityType)
+                         .Invoke(modelBuilder.Configurations, new object[] { entityConfig });
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #endregion Methods
     }
 }
---
+++ SourceCodes/Aliencube.DataEntities/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.DataEntities")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Aliencube.DataEntities")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("86ac112f-c2d1-4b79-aa0c-9f5610477ad0")]
